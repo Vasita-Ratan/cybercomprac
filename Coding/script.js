89ee14700cdd4d4b05a6448f695567a4fe09424c
vasitaratan123@gmail.com <vasitaratan123@gmail.com>
2021-01-09 19:01:32 +0530

    script added

---
+++ Coding/script.js
@@ -0,0 +1,325 @@
+//Date 8/1/21
+										
+/*//1)variable naming rules
+
+//script variable define in index file 
+console.log(script);
+
+//variables define lower and using underscore
+var firstname = 'ratan';
+console.log(firstname);
+
+
+//if define veriables value return on console true/false
+var fullage = true;
+console.log(fullage);
+
+//without value just defining variable this is call undefined variable
+
+var age;
+console.log(log);
+
+//without datatype define value variable
+
+name = 'Vasita Ratan';
+console.log(name);
+
+//this type of datatypes breaking rules of javascript notation returning error unexpected this datatype
+//remove error use underscore or remove back slash
+var _5year = 3;
+var forkwork = 'maintain';
+
+//keywords can't use as variable name
+
+var function = 'Ratan';
+
+var delete = 'ratam';
+
+var if = 'ratan';
+
+
+//2)concetenate two variables
+
+var firstname = 'ratan';
+var age = 20;
+console.log(firstname + ' ' + age);
+
+
+//type coercion
+var job,ismerried;
+job = 'engineer';
+ismerried = false;
+
+console.log(firstname + 'is a'+ age + 'older' + job + 'merried or not' + ismerried);
+
+
+//3)type mutaion
+
+//alert showing the beautiful popup message
+
+alert(firstname + 'is a'+ age + 'older' + job + 'merried or not' + ismerried);
+
+//4)prompting the popup message 
+
+var lastname = prompt(firstname + 'is a'+ lastname);
+
+
+//5) Javascript basic operators
+
+var years = 2018-10;
+console.log(years);
+ 
+//another way to define 
+
+var year = 2020;
+var ans  = year - 20;
+var ans = year - 9;
+
+console.log(ans);
+
+var ans;
+current = 2020;
+var ratanage=20;
+var meetage=22;
+
+ans = current - 20;
+
+//using arithmetic operator
+
+console.log(current * 2);
+console.log(current + 2);
+console.log(current - 2);
+console.log(current / 10);
+
+
+//logical operator 
+//ans returning in console true/false
+
+var oldage = ratanage < meetage; 
+console.log(oldage);
+
+//typeof operator
+
+
+console.log(typeof ratanage); // returning age in number 
+
+console.log(typeof ismerried); //returing boolean value true/
+
+//undefine function
+var none;
+
+console.log(typeof none); 
+
+//operator procedure
+
+var now = 2018;
+var yearage = 2000;
+var fullage = 20;
+
+var isfullage = now -fullage >= fullage;//true
+console.log(isfullage);
+
+
+//define operator many way
+
+var ageratan = now - yearage;
+var agemark = 35;
+var agemake = (ageratan + agemark) / 2;
+console.log(agemake);
+
+
+//multiple assignments
+
+var x,y;
+x =y = (3 + 5) * (4 - 6)//8 * 4 - 6 //32 - 6//26
+
+console.log(x,y);
+
+  //more operators infix or prefix refering stackoverflow many rules
+
+  x += 2;
+  console.log(x);
+
+  x *=20;
+  console.log(x);
+
+  x++;
+
+console.log(x);
+
+
+*/
+
+
+                                                       //PRACTICE JAVASCRIPT :-1//
+
+//Q.1)create variables:-
+	var mass,height;
+
+	mass=markkg = 70;
+	hegiht=markmeter = 155;
+
+	mass=johnkg = 68;
+	heeight=johnmeter = 152;
+
+	//console.log(markkg,markmeter,johnkg,johnmeter);
+	console.log('markkg:-'+ markkg +'mark height in meter'+ markmeter + 'johnkg is:-' + johnkg + 'john hegiht in meter' + johnmeter);
+
+
+	//Q.2)calculate both Bmi:
+
+	var markBmi = markkg / (markmeter * markmeter)
+	console.log('mark BMI is:-' + markBmi); 
+	//console.log(markheight*markheight + 'mark height Meter');
+	var johnBmi = johnkg / (johnmeter * johnmeter)
+	console.log('John BMI is:-' +  johnBmi); 
+
+
+	//Q.3) comparing both Bmi:
+
+	var comapare = markBmi < johnBmi;
+
+	console.log(comapare);
+
+	//Q.4) prompting the message beautiful message:-
+
+	var com = prompt('markBmi greater then johnBmi' + comapare );
+	console.log(com);
+
+
+
+                                                        //PRACTICE JAVASCRIPT :-2
+
+
+//Q.1 find the average of two different teams 
+
+var john,mike;
+john = 89,120,103;
+mike = 116,94,123;
+
+var johnavg = 89 + 120 + 103 / 3;
+console.log('john team average score is:-' + johnavg);
+
+var mikeavg = 116 + 94 + 123 / 3;
+console.log('mike team average score is:-' + mikeavg);
+
+
+//Q.2 find the which one team average score is higher score print on console:
+if ( johnavg > mikeavg)
+{
+		console.log('john team score ' + johnavg +  'higher then mike team');
+
+}
+else
+{
+		console.log('mike team score ' +    mikeavg   +  '  higher then john team');
+
+
+}
+
+//Q.3 just change score and check who is winner
+
+var john,mike;
+john = 116,94,123;
+mike = 89,120,103;
+
+var johnavg = 116 + 94 + 123 / 3;
+console.log('john team average score is:-' + johnavg);
+
+var mikeavg = 89 + 120 + 103 / 3;
+console.log('mike team average score is:-' + mikeavg);
+
+
+
+
+//Q.2 find the which one team average score is higher score print on console:
+if ( johnavg > mikeavg)
+{
+		console.log('john team score ' + johnavg +  ' higher then mike team');
+
+}
+else
+{
+		console.log('mike team score ' +    mikeavg   +  '  higher then john team');
+
+
+}
+
+
+//Q.4 add one more team name is mary
+
+var mary;
+mary = 97,134,105;
+var maryavg = 97 + 134 + 105 / 3 ;
+console.log('mary average score is:- '  + maryavg);
+
+//comparing with john and mike average score
+
+if(johnavg > mikeavg)
+{
+	alert('john team score ' + johnavg +  ' higher then mike and mary team');
+	console.log('john team average score is:-' + johnavg);
+
+}
+else
+{
+	alert('mike team score ' +    mikeavg   +  '  higher then john and mary team');
+	console.log('mike team average score is:-' + mikeavg);
+}
+if(maryavg > johnavg)
+{
+	alert('mary team score ' +    maryavg   +  '  higher then john and mike team');
+	console.log('mary average score is:- '  + maryavg);
+}
+else
+{
+	alert('please try again!!!');
+}
+
+
+
+
+//Q.5 changing the score of all teams then check again
+
+
+var mike,mary,john;
+mike = 97,134,105;
+mary = 116,94,123;
+john = 89,120,103;
+
+var mikeavg = 97 + 134 + 105 / 3 ;
+console.log('mike average score is:- '  + mikeavg);
+
+var maryavg = 116 + 94 + 123 / 3;
+console.log('mary team average score is:-' + maryavg);
+
+var johnavg = 89 + 120 + 103 / 3;
+console.log('john team average score is:-' + johnavg);
+
+if(maryavg > johnavg)
+{
+	alert('mary team score ' +    maryavg   +  '  higher then john and mike team');
+	console.log('mary team average score is:-' + maryavg);
+	
+
+}
+else
+{
+	alert('john team score ' + johnavg +  ' higher then mike and mary team');
+	console.log('john team average score is:-' + johnavg);
+}
+if(maryavg < mikeavg)
+{
+	alert('mike team score ' +    mikeavg   +  '  higher then john and mike team');
+	console.log('mike average score is:- '  + mikeavg);
+}
+else
+{
+	alert('please try again!!!');
+}
+
+
+
+
+
+
